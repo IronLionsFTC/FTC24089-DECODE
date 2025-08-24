d9b4bbe00b61a5c2a5d665fd05dd543c32102640
Harrison Chapman <hchap1@hotmail.com>
2025-08-24 20:39:56 +1000

    feat: Lion Motor may own multiple motors to use identically

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lioncore/hardware/LionMotor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lioncore/hardware/LionMotor.java
@@ -4,30 +4,46 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LionMotor {
-    private DcMotor motor;
+    private List<DcMotor> motors;
     private double position;
     private double power;
-    private boolean hasEncoder;
 
-    private LionMotor(HardwareMap hardwareMap, String name, boolean hasEncoder) {
-        this.motor = hardwareMap.get(DcMotor.class, name);
+    private LionMotor(HardwareMap hardwareMap, String... names) {
+        this.motors = new ArrayList<>();
+        for (String name : names) {
+            this.motors.add(hardwareMap.get(DcMotor.class, name));
+        }
         this.position = 0;
         this.power = 0;
-        this.hasEncoder = hasEncoder;
     }
 
     public static LionMotor withoutEncoder(HardwareMap hardwareMap, String name) {
-        return new LionMotor(hardwareMap, name, false);
+        return new LionMotor(hardwareMap, name);
     }
 
     public static LionMotor withEncoder(HardwareMap hardwareMap, String name) {
-        return new LionMotor(hardwareMap, name, true);
+        return new LionMotor(hardwareMap, name);
+    }
+
+    /**
+     * A LionMotor which operates multiple motors at once using the encoder of a singular motor.
+     * @param hardwareMap The hardwaremap
+     * @param names Any number of motors
+     * @return
+     */
+    public static LionMotor masterSlaves(HardwareMap hardwareMap, String... names) {
+        return new LionMotor(hardwareMap, names);
     }
 
     public void setPower(double power) {
         this.power = power;
-        this.motor.setPower(power);
+        for (DcMotor motor : this.motors) {
+            motor.setPower(power);
+        }
     }
 
     public double getPower() {
@@ -35,26 +51,32 @@ public class LionMotor {
     }
 
     public double getPosition() {
-        if (this.hasEncoder) {
-            return this.motor.getCurrentPosition();
-        } else {
-            return 0.0;
-        }
+        if (this.motors.isEmpty()) return 0.0;
+        this.position = this.motors.get(0).getCurrentPosition();
+        return this.cachedPosition();
+    }
+
+    public double cachedPosition() {
+        return this.position;
     }
 
     public void resetPosition() {
-        if (this.hasEncoder) {
-            this.motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            this.motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        if (this.motors.isEmpty()) return;
+        this.motors.get(0).setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        this.motors.get(0).setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    }
+
+    public void setReversed(boolean... reversed) {
+        assert reversed.length == motors.size();
+        for (int idx = 0; idx < this.motors.size(); idx++) {
+            if (reversed[idx]) this.motors.get(idx).setDirection(DcMotorSimple.Direction.REVERSE);
+            else this.motors.get(idx).setDirection(DcMotorSimple.Direction.FORWARD);
         }
     }
 
-    public void setReversed(boolean reversed) {
-        if (reversed) this.motor.setDirection(DcMotorSimple.Direction.REVERSE);
-        else this.motor.setDirection(DcMotorSimple.Direction.FORWARD);
-    }
-
     public void setZPB(DcMotor.ZeroPowerBehavior zpb) {
-        this.motor.setZeroPowerBehavior(zpb);
+        for (DcMotor motor : this.motors) {
+            motor.setZeroPowerBehavior(zpb);
+        }
     }
 }
